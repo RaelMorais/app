25cce08e2969b629b98a67335a44b1b06cdda941
RaelMorais <israelsantanamorais@outlook.com>
2026-04-18 17:37:31 -0300

    fix: error in redirect

--- app/page.tsx
+++ app/page.tsx
@@ -4,5 +4,5 @@ export default function RootPage({ params }: { params: { locale: string } }) {
   
   const currentLocale = params?.locale || 'pt'; 
 
-  redirect(`/${currentLocale}/errors/404`);
+  redirect(`/${currentLocale}/home`);
 }
